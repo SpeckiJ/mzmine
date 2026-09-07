1e458e74fd38f2b38d80fd7d8c0eb8fbc612a1ca
speckij <jan.speckamp@mzio.io>
2026-09-07 17:58:58 +0200

    fix getTICValue()

--- mzmine-community/src/main/java/io/github/mzmine/datamodel/data_access/MobilityScanDataAccess.java
+++ mzmine-community/src/main/java/io/github/mzmine/datamodel/data_access/MobilityScanDataAccess.java
@@ -421,9 +421,9 @@ public class MobilityScanDataAccess implements MobilityScan {
     return eligibleFrames;
   }
 
-  @Nullable
   @Override
-  public Double getTIC() {
+  public double getTICValue() {
+    // TODO: this could be precomputed once
     return ArrayUtils.sum(intensities, 0, currentNumberOfDataPoints);
   }
 
