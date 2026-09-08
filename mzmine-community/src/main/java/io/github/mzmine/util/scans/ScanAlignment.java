d14d8b017c2f6d82c8ec0489b748ecfcd53e41af
speckij <jan.speckamp@mzio.io>
2026-09-08 15:44:01 +0200

    reuses existing utility code in `findMatchModAware`
    Co-authored-by: steffen <steffen.heuckeroth@mzio.io>

--- mzmine-community/src/main/java/io/github/mzmine/util/scans/ScanAlignment.java
+++ mzmine-community/src/main/java/io/github/mzmine/util/scans/ScanAlignment.java
@@ -251,26 +251,19 @@ public class ScanAlignment {
    */
   private static DataPoint findMatchModAware(MZTolerance mzTol, DataPoint dpa,
       List<DataPoint> sortedB, double precursorMzA, double precursorMzB) {
-    final double deltaMZ = precursorMzB - precursorMzA;
     final double mzA = dpa.getMZ();
-    final double mzAShifted = mzA + deltaMZ;
-    final double ppmTolerance = mzTol.getPpmTolerance();
-    final double mzToleranceAbsolute = mzTol.getMzTolerance();
-    // We roll our own checkWithinTolerance() here to precompute as much as possible.
-    final double mzToleranceRelativeA = mzA / 1_000_000.0 * ppmTolerance;
-    final double mzToleranceRelativeB = mzAShifted / 1_000_000.0 * ppmTolerance;
+    final double mzAShifted = mzA + (precursorMzB - precursorMzA);
+    final double mzTolerance = mzTol.getMzToleranceForMass(mzA);
+    final double mzToleranceShifted = mzTol.getMzToleranceForMass(mzAShifted);
+
     for (int i = 0; i < sortedB.size(); i++) {
       final DataPoint dpb = sortedB.get(i);
       // TODO how to handle cases where we have both the direct fragment and the modified fragment
       // as in shifted by the precursor m/z currently we just use the first one we find
-
       final double distA = Math.abs(mzA - dpb.getMZ());
       final double distB = Math.abs(mzAShifted - dpb.getMZ());
 
-      if (distA <= mzToleranceAbsolute
-          || distA <= mzToleranceRelativeA
-          || distB <= mzToleranceAbsolute
-          || distB <= mzToleranceRelativeB) {
+      if (distA <= mzTolerance || distB <= mzToleranceShifted) {
         // remove from list and return
         sortedB.remove(i);
         return dpb;
@@ -278,7 +271,6 @@ public class ScanAlignment {
     }
     return null;
   }
-
 
   public static double getTIC(DataPoint[] scan) {
     return Arrays.stream(scan).mapToDouble(DataPoint::getIntensity).sum();
